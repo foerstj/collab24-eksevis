4fad26692278b68de02ef2372394c00f9400c579
Johannes Förstner <johannes@famfoerstner.de>
2024-08-03 17:24:42 +0200

    Add repo build scripts

---
+++ build.bat
@@ -0,0 +1,31 @@
+chcp 65001
+:: name of map
+set map=Collab24
+set res=%map%
+:: name of map, case-sensitive
+set map_cs=Collab 24 Eksevis
+:: tank properties
+set year=2024
+set copyright=CC-BY-SA %year%
+set author=Eksevis
+set title=%map_cs%
+
+:: path of Bits dir
+set bits=%~dp0.
+:: path of DS installation
+set ds=%DungeonSiege%
+:: path of TankCreator
+set tc=%TankCreator%
+
+:: Compile map file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /S
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "%copyright%" -title "%title%" -author "%author%"
+if %errorlevel% neq 0 pause
+
+:: Compile main resource file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%bits%\art" "%tmp%\Bits\art" /S
+robocopy "%bits%\world\contentdb\templates" "%tmp%\Bits\world\contentdb\templates" /S
+"%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
+if %errorlevel% neq 0 pause
