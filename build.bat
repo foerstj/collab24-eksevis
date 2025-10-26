b5a8d03d507fc7448564ff4256c78b7c436be6d8
Johannes Förstner <johannes@famfoerstner.de>
2025-10-26 10:00:15 +0100

    Move all templates to a subfolder

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 :: name of map
 set map=collab24-eksevis
+set res=collab24
 :: name of map, case-sensitive
 set map_cs=Collab 24 Eksevis
 :: tank properties
@@ -63,7 +64,7 @@ rmdir /S /Q "%tmp%\Bits"
 robocopy "%bits%\art" "%tmp%\Bits\art" /S
 robocopy "%bits%\ui" "%tmp%\Bits\ui" /S
 robocopy "%bits%\world\ai" "%tmp%\Bits\world\ai" /S
-robocopy "%bits%\world\contentdb\templates\regular" "%tmp%\Bits\world\contentdb\templates\regular" /S
+robocopy "%bits%\world\contentdb\templates\%res%" "%tmp%\Bits\world\contentdb\templates\%res%" /S
 robocopy "%bits%\world\global" "%tmp%\Bits\world\global" /S
 "%tc%\RTC.exe" -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "%copyright%" -title "%title%" -author "%author%"
 if %errorlevel% neq 0 pause
